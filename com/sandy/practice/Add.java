48fccb0c1c411bc33b2f564d456e48b338dc2f67
MeAmatuer <sandipan2602@gmail.com>
2019-09-05 00:10:25 +0530

    first commit

---
+++ com/sandy/practice/Add.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Add{
+	public static void main(String[] args){
+		int firstNum, secondNum;
+		Scanner sc = new Scanner(System.in);
+		firstNum = sc.nextInt();
+		secondNum = sc.nextInt();
+		System.out.println(firstNum+secondNum);
+	}
+}
